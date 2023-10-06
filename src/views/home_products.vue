3a3ee6c7296161fb8193f4e35add9dd0b932e2a0
khatatong308 <khatatong47949@gmail.com>
2023-10-06 21:10:10 +0700

    help me pls

---
+++ src/views/home_products.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h1>รายการสินค้า</h1>
+    <div class="product-list">
+      <div class="product-card" v-for="product in products" :key="product.id">
+        <img :src="product.image" :alt="product.name">
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>ราคา: ฿{{ product.price }}</p>
+        <div>
+          <button @click="addToCart(product)">เพิ่มลงในตะกร้า</button>
+          <button @click="removeFromCart(product)" :disabled="product.cartQuantity === 0">ลดจากตะกร้า</button>
+          <span>{{ product.cartQuantity }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  created() {
+    for (let i = 1; i <= 10; i++) {
+      this.products.push({
+        id: i,
+        name: `สินค้าที่ ${i}`,
+        description: `รายละเอียดสินค้าที่ ${i}`,
+        price: (i * 10).toFixed(2), 
+        image: `url_to_image_${i}.jpg`, 
+        cartQuantity: 0, // เพิ่ม cartQuantity ในสินค้า
+      });
+    }
+  },
+  methods: {
+    addToCart(product) {
+      product.cartQuantity++;
+    },
+    removeFromCart(product) {
+      if (product.cartQuantity > 0) {
+        product.cartQuantity--;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.product-card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  width: 200px;
+  text-align: center;
+}
+
+button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/views/product_detail.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+      <h1>รายการสินค้า</h1>
+      <div class="product-list">
+        <div class="product-card" v-for="product in products" :key="product.id">
+          <img :src="product.image" :alt="product.name">
+          <h2>{{ product.name }}</h2>
+          <p>{{ product.description }}</p>
+          <p>ราคา: ฿{{ product.price }}</p>
+          <p>จำนวน: {{ product.quantity }}</p>
+          <button @click="addToCart(product)">เพิ่มลงในตะกร้า</button>
+          <button @click="removeFromCart(product)">ลดจำนวน</button>
+        </div>
+      </div>
+      <h2>ราคารวมในตะกร้า: ฿{{ calculateTotal() }}</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        products: [],
+        cart: [], 
+      };
+    },
+    created() {
+      
+      for (let i = 1; i <= 10; i++) {
+        this.products.push({
+          id: i,
+          name: `สินค้าที่ ${i}`,
+          description: `รายละเอียดสินค้าที่ ${i}`,
+          price: (i * 10).toFixed(2),
+          image: `url_to_image_${i}.jpg`,
+          quantity: 0, 
+        });
+      }
+    },
+    methods: {
+      addToCart(product) {
+        // เพิ่มสินค้าลงในตะกร้าโดยเพิ่มจำนวนสินค้าในรายการสินค้า
+        product.quantity++;
+      },
+      removeFromCart(product) {
+        // ลดจำนวนสินค้าในตะกร้า
+        if (product.quantity > 0) {
+          product.quantity--;
+        }
+      },
+      calculateTotal() {
+        // คำนวณราคารวมในตะกร้า
+        return this.cart.reduce((total, product) => total + product.price * product.quantity, 0);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* เพิ่มสไตล์ CSS ของการ์ดสินค้าที่นี่ */
+  .product-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  
+  .product-card {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 10px;
+    width: 200px;
+    text-align: center;
+  }
+
+  
+  </style>
+  
